978a5e9551f276f84a5d2a4ef8d04038ffef73f0
Mitchell Simoens <ms.simoens@gmail.com>
2011-03-11 14:27:35 -0500

    Initial Commit

---
+++ Ext.plugin.FormEnterSubmit.js
@@ -0,0 +1,22 @@
+Ext.define("Ext.plugin.FormEnterSubmit", {
+	extend: "Ext.AbstractPlugin",
+	alias: "plugin.formentersubmit",
+
+	submitFn : Ext.emptyFn,
+
+	init: function(form) {
+		var me = this,
+			items = form.items;
+
+		items.each(function(item) {
+			item.on("specialkey", me.handleSubmitFunc, me);
+		}, me);
+	},
+
+	handleSubmitFunc: function(field, e) {
+		var me = this;
+		if (e.getKey() == e.ENTER) {
+			Ext.bind(me.submitFn, me.cmp)();
+		}
+	}
+});
